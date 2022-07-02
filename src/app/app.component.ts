1b47c39e1b0fc873d4660790977daa826c1e7dd7
Anastasia Skripnik <scripnik.a21@cf.dnu.dp.ua>
2022-07-02 03:19:22 +0300

    add pause functional

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { interval, VirtualTimeScheduler } from 'rxjs';
+import { interval, fromEvent, debounceTime } from 'rxjs';
+import { delay, buffer, filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit{
- 
+export class AppComponent implements OnInit {
   title = 'stopwatch-app';
 
   min: any = '0' + 0;
@@ -16,39 +16,63 @@ export class AppComponent implements OnInit{
   startTimer: any;
   isRunning: boolean = false;
   timeCounter$ = interval(1000);
- 
-  ngOnInit(): void {
-    this.isRunning = false; 
-    
-  }  
 
-  start(): void { 
-    console.log('START TIMER, isrun = ', this.isRunning)
-    if(!this.isRunning) { 
-      this.isRunning = true;
-      this.startTimer = this.timeCounter$.subscribe((sec:any)=> {
-        console.log(sec)
-        this.sec++
-        this.sec = this.sec < 10 ? "0" + this.sec : this.sec
-          if(this.sec === 60)
-          { 
-            this.min++;
-            this.min = this.min < 10 ? "0" + this.min : this.min
-            this.sec = '0' + 0
-          }
-      })
+  resetButton = document.getElementsByClassName('btn-wait');
+  onWait$ = fromEvent(this.resetButton, 'click');
+
+  ngOnInit(): void {
+    this.isRunning = false;
+    this.onWait$
+      .pipe(
+        buffer(this.onWait$.pipe(debounceTime(500))),
+        map(clickArray=>clickArray.length),
+        filter((len) => len === 2)
+      )
+      .subscribe((event) => this.wait());
+
+      this.onWait$.pipe(
+      delay(1000))
+      .subscribe(suggestion => {
+        console.log('suggestion');
+      });  
+  }
+  wait(): void {
+    console.log('Double Click!');
+    if (this.isRunning) {
+      console.log('wait');
     }
   }
 
-  stop(): void { 
-    this.sec = '0' + 0; 
-    this.isRunning = false;
-    this.startTimer.unsubscribe()
+  start(): void {
+    console.log('START TIMER, isrun = ', this.isRunning);
+    if (!this.isRunning) {
+      this.isRunning = true;
+      this.startTimer = this.timeCounter$.subscribe((sec: any) => {
+        this.sec++;
+        this.sec = this.sec < 10 ? '0' + this.sec : this.sec;
+        if (this.sec === 60) {
+          this.min++;
+          this.min = this.min < 10 ? '0' + this.min : this.min;
+          this.sec = '0' + 0;
+        }
+      });
+    }
   }
 
-  reset(): void { 
-    /* this.sec = '0' + 0; 
+  stop(): void {
+    this.resetVars();
     this.isRunning = false;
-    this.startTimer.unsubscribe() */
+    if (this.startTimer) {
+      this.startTimer.unsubscribe();
+    }
+  }
+
+  reset(): void {
+    this.resetVars();
+  }
+
+  resetVars(): void {
+    this.sec = '0' + 0;
+    this.min = '0' + 0;
   }
 }
